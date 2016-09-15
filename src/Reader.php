05e8a7e457993ca8feb92d0820b7226f359060d3
Martin Hughes <martinph01@gmail.com>
2016-09-15 01:28:49 +0100

    Reader construct

--- src/Reader.php
+++ src/Reader.php
@@ -1,11 +1,51 @@
 <?php
 
-namespace "Choccybiccy\Mobi";
+namespace Choccybiccy\Mobi;
 
 /**
- * Class Reader
+ * Class Reader.
  */
 class Reader
 {
+    /**
+     * @var \SplFileObject
+     */
+    protected $file;
+
+    /**
+     * Reader constructor.
+     * @param string|\SplFileObject $file
+     */
+    public function __construct($file)
+    {
+        if (is_string($file)) {
+            $file = new \SplFileObject($file, "r");
+        }
+        $this->file = $file;
+    }
+
+    /**
+     * @param \SplFileObject $file
+     * @return static
+     */
+    public function createFromFileObject(\SplFileObject $file)
+    {
+        return new static($file);
+    }
+
+    /**
+     * Validate and return a string.
+     *
+     * @param $string
+     *
+     * @return string
+     */
+    public static function validateString($string)
+    {
+        if (is_string($string) || (is_object($string) && method_exists($string, '__toString'))) {
+            return (string) $string;
+        }
+        throw new \InvalidArgumentException('Expected data must either be a string or stringable');
+    }
 }
 
---
+++ tests/ReaderTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Choccybiccy\Mobi;
+
+/**
+ * Class ReaderTest.
+ */
+class ReaderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConstruct()
+    {
+        $mobi = new Reader(new \SplFileObject($this->getMobiPath()));
+        var_dump($mobi);die();
+    }
+
+    /**
+     * @return string
+     */
+    protected function getMobiPath()
+    {
+        return realpath(__DIR__ . '/../resources/The_Adventures_of_Sherlock_Holmes_by_Doyle.mobi');
+    }
+
+    /**
+     * @param array|null $methods
+     * @return \PHPUnit_Framework_MockObject_MockObject|Reader
+     */
+    protected function getMockReader(array $methods = null)
+    {
+        return $this->getMockBuilder(Reader::class)
+            ->setMethods($methods)
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+}
+
