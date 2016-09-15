55ab98301c7cae1914d256de34ca65c725b1b8ce
Martin Hughes <martin.hughes@uk.aql.com>
2016-09-15 11:06:05 +0100

    PalmDB heade

---
+++ src/Header/Record/PalmRecord.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Choccybiccy\Mobi\Header\Record;
+
+/**
+ * Class PalmRecord.
+ */
+class PalmRecord
+{
+    /**
+     * @var int
+     */
+    protected $offset;
+
+    /**
+     * @var int
+     */
+    protected $attributes;
+
+    /**
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * PalmRecord constructor.
+     * @param int $offset
+     * @param int $attributes
+     * @param int $id
+     */
+    public function __construct($offset, $attributes, $id)
+    {
+        $this->offset = $offset;
+        $this->attributes = $attributes;
+        $this->id = $id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getOffset()
+    {
+        return $this->offset;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAttributes()
+    {
+        return $this->attributes;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
--- src/Reader.php
+++ src/Reader.php
@@ -2,6 +2,10 @@
 
 namespace Choccybiccy\Mobi;
 
+use Choccybiccy\Mobi\Exception\InvalidFormatException;
+use Choccybiccy\Mobi\Header\PalmDb;
+use Choccybiccy\Mobi\Header\Record\PalmRecord;
+
 /**
  * Class Reader.
  */
@@ -13,6 +17,16 @@ class Reader
     protected $file;
 
     /**
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @var PalmDb
+     */
+    protected $palmDb;
+
+    /**
      * Reader constructor.
      * @param string|\SplFileObject $file
      */
@@ -22,6 +36,34 @@ class Reader
             $file = new \SplFileObject($file, "r");
         }
         $this->file = $file;
+        $this->palmDb = new PalmDb();
+        $this->parse();
+    }
+
+    /**
+     * Parse the file data
+     */
+    protected function parse()
+    {
+        $file = $this->file;
+        $file->fseek(60);
+        $content = $file->fread(8);
+        if ($content !== 'BOOKMOBI') {
+            throw new InvalidFormatException('The file is not a valid mobi file');
+        }
+
+        $file->fseek(76);
+        $content = $file->fread(2);
+        $records = hexdec(bin2hex($content));
+
+        $file->fseek(78);
+        for ($i=0; $i<$records; $i++) {
+            $this->palmDb->addRecord(new PalmRecord(
+                hexdec(bin2hex($file->fread(4))),
+                hexdec(bin2hex($file->fread(1))),
+                hexdec(bin2hex($file->fread(3)))
+            ));
+        }
     }
 
     /**
